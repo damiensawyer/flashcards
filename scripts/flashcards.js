e725ceba2c5edc94fae1c33be86c1914bb665fbf
Damien Sawyer <damiensawyer@gmail.com>
2015-09-20 22:57:38 +1000

    working

--- scripts/flashcards.js
+++ scripts/flashcards.js
@@ -1,19 +1,24 @@
 /// <reference path="types/knockout.d.ts"/>
 /// <reference path="types/underscore.d.ts"/>
+// DON'T FORGET TO COMPILE THE TYPESCRIPT. There is no grunt / gulp watcher
 var FlashCardPageViewModel = (function () {
     function FlashCardPageViewModel() {
         var _this = this;
-        this.sourceText = ko.observable('yes');
-        this.wordList = ko.computed(function () {
+        this.sourceText = ko.observable('hello this is a test of using superfulously long words');
+        this.distinctList = ko.computed(function () {
             var s = _this.sourceText();
             var fullList = s.replace(/\./g, '')
                 .replace(/\,/g, '')
                 .replace(/\s+/g, ' ')
                 .split(' ');
-            var distinctList = _.sortBy(_.uniq(fullList))
-                .join(' ');
+            var distinctList = _.sortBy(_.uniq(fullList));
             return distinctList;
         });
+        this.wordList = ko.computed(function () {
+            return _this.distinctList()
+                .join(' ');
+        });
+        this.outputLabel = ko.computed(function () { return "Here are the list of " + _this.wordList().split(' ').length + " unique words."; });
     }
     return FlashCardPageViewModel;
 })();
--- scripts/flashcards.ts
+++ scripts/flashcards.ts
@@ -1,10 +1,13 @@
 /// <reference path="types/knockout.d.ts"/>
 /// <reference path="types/underscore.d.ts"/>
 
+
+// DON'T FORGET TO COMPILE THE TYPESCRIPT. There is no grunt / gulp watcher
 class FlashCardPageViewModel
 {
-	sourceText = ko.observable<string>('yes');
-	wordList = ko.computed<string>(() => {
+	sourceText = ko.observable<string>('hello this is a test of using superfulously long words');
+
+	distinctList = ko.computed<string[]>(() => {
 		var s = this.sourceText();
 		var fullList = s.replace(/\./g, '')
 		.replace(/\,/g, '')
@@ -12,12 +15,18 @@ class FlashCardPageViewModel
 		.split(' ');
 
 		var distinctList = _.sortBy(_.uniq(fullList))
-		.join(' ');
-
-
+		
 		return distinctList;
 
 		});
+
+	wordList = ko.computed<string>(() => {
+		return this.distinctList()
+		.join(' ');
+
+		});
+
+	outputLabel = ko.computed(()=>"Here are the list of " + this.wordList().split(' ').length + " unique words.");
 }
 
 
